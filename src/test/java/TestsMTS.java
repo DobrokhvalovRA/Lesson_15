4555838acdc3d92891b61809aa84b54d999fa595
DobrokhvalovRA <DobrokhvalovRA@icloud.com>
2025-01-27 10:34:35 +0300

    Update TestsMTS.java

--- src/test/java/TestsMTS.java
+++ src/test/java/TestsMTS.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
@@ -12,40 +11,41 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestsMTS {
-    private static WebDriver driver;
+    private static WebDriver driver = null;
 
     @BeforeClass
     public static void make() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.mts.by/");
-        //соглашаемся на куки
-        driver.findElement(By.xpath("/html/body/div[6]/main/div/div[2]/div/div[2]/button[3]")).click();
+        driver.findElement(By.id("cookie-agree")).click();
     }
 
-    @Test //проверяем заголовок
+    @Test
     public void checkTitle() {
         String title;
 
         // что делать с этим ? На сколько я понял он же должен запустится сам перед тестами,
         //если будет не сложно поясните
         make();
-
         title = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/h2")).getText();
         assertTrue(title.equals("Онлайн пополнение\n" + "без комиссии"));
+
     }
 
-    @Test // проверяем логотипы
-    //Буду откровенен тут мне тоже не понятно, если есть xpath значит элемент есть. Что тогда проверять.
-    // Или надо проверить список с этими элементами ?
+    @Test
     public void checkLogo() throws NoSuchElementException {
-
-
-        WebElement visa = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]/ul/li[1]/img"));
+        /*WebElement visa = driver.findElement(By.);
         WebElement vvisa = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]/ul/li[2]/img"));
         WebElement mc = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]/ul/li[3]/img"));
         WebElement mcsc = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]/ul/li[4]/img"));
-        WebElement bc = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]/ul/li[5]/img"));
+        WebElement bc = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/div[2]/ul/li[5]/img"));*/
+
+        WebElement visa = driver.findElement(By.cssSelector("img[alt = 'Visa']"));
+        WebElement vvisa = driver.findElement(By.cssSelector("img[alt = 'Verified By Visa']"));
+        WebElement mc = driver.findElement(By.cssSelector("img[alt = 'MasterCard']"));
+        WebElement mcsc = driver.findElement(By.cssSelector("img[alt = 'MasterCard Secure Code']"));
+        WebElement bc = driver.findElement(By.cssSelector("img[alt = 'Белкарт']"));
 
         assertTrue(visa.isDisplayed());
         assertTrue(vvisa.isDisplayed());
@@ -54,20 +54,29 @@ public class TestsMTS {
         assertTrue(bc.isDisplayed());
     }
 
-    @Test //проверяем ссылку
-    // или надо проверить что она открыла ?
+    @Test
     public void checkLink() {
-        WebElement link = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/a"));
+        //WebElement link = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[4]/div[1]/div/div/div[2]/section/div/a"));
+        WebElement link = driver.findElement(By.linkText("Подробнее о сервисе"));
         assertTrue(link.isEnabled());
+        assertTrue(link.getAttribute("href") != null);
+        link.click();
+        driver.navigate().back();
+
     }
 
-    @Test //проверка кнопки продолжить
+    @Test
     public void checkButton() {
         WebElement selector = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/div[1]/div[1]/div[2]/button"));
-        WebElement number = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
-        WebElement sum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
-        WebElement email = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        //WebElement selector = driver.findElement(By.className("select_header"));
+        //WebElement number = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
+        WebElement number = driver.findElement(By.id("connection-phone"));
+        //WebElement sum = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
+        WebElement sum = driver.findElement(By.id("connection-sum"));
+        //WebElement email = driver.findElement(By.xpath("//*[@id=\"connection-email\"]"));
+        WebElement email = driver.findElement(By.id("connection-email"));
         WebElement continueBut = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
+        //WebElement continueBut = driver.findElement(By.className("button button_default"));
 
         //assertTrue(number.isEnabled());
         number.click();
